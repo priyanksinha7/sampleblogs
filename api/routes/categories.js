96208d448a23d389b62919aa09a688a77ec9912c
priyanksinha7 <priyanksinha63@gmail.com>
2023-01-26 23:37:19 +0530

    newchanges

--- api/routes/categories.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { newCategory,findCategory } = require("../controllers/categoriesController");
-const Category = require("../models/Category");
-
-router.post("/",newCategory);
-
-router.get("/", findCategory);
-
-module.exports = router;
